829e4927f49f357bef56aa57076a5e50de9e8bcd
poundinger <nithi@umich.edu>
2020-11-06 10:53:55 +0700

    Test new julia code

---
+++ hello_world.jl
@@ -0,0 +1,21 @@
+# press Alt + Enter to run the code
+print("Hello world!")
+
+2+3
+
+## this is a code cell
+# press Shift + Enter to execute a code.
+println("Hello.")
+println("Test second line.")
+##
+
+## this is another code cell
+println(42)
+##
+
+#%% Run a block of code together
+println("hello")
+println("here")
+println("I am.")
+#%%
+# Note: The prints method simply print text on the console and does not add any new line. While println adds new line after print text on console.
---
+++ python_interaction.jl
@@ -0,0 +1,8 @@
+# using Pkg
+# Pkg.add("PyCall")
+# using Pycall
+
+##
+np = pyimport("numpy")
+np.sin(3)
+##
